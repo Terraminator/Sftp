02dd5c9122f2dbca0484ebbbdbd662b97c1e81ed
Terraminator <49759824+Terraminator@users.noreply.github.com>
2021-08-30 21:42:56 +0200

    Add files via upload

--- sftp.py
+++ sftp.py
@@ -2,32 +2,48 @@ import paramiko
 import threading
 from time import sleep
 import sys
+import os
 
 ssh = paramiko.SSHClient()
+def banner():
+	os.system("color a")
+	print(r"""\
 
-target = str(input("Target: "))
+.___________..______          ___      .__   __.      _______. _______  _______ .______      
+|           ||   _  \        /   \     |  \ |  |     /       ||   ____||   ____||   _  \     
+`---|  |----`|  |_)  |      /  ^  \    |   \|  |    |   (----`|  |__   |  |__   |  |_)  |    
+    |  |     |      /      /  /_\  \   |  . `  |     \   \    |   __|  |   __|  |      /     
+    |  |     |  |\  \----./  _____  \  |  |\   | .----)   |   |  |     |  |____ |  |\  \----.
+    |__|     | _| `._____/__/     \__\ |__| \__| |_______/    |__|     |_______|| _| `._____|
+                                                                                             
+                                                                         
+
+					""")                                                                                                                                                                                              
+
+target = "192.168.178.25"
+banner()
 uname = input("Username: ")
 pwd = input("Password: ")
 def datatransfer(target, uname, pwd):
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	ssh.connect(target, username=uname, password=pwd)
 		
-	reply = input("Command: ")
-
+	reply = input("Befehl: ")
+	print(reply)
 	if reply == "Get" or reply == "get":
-		item = input("File: ")
-		dst = item
+		item = input("Datei: ")
+		Ziel = item
 		ftp_client=ssh.open_sftp()
-		ftp_client.get(item, dst)
+		ftp_client.get(item, Ziel)
 		ftp_client.close()
 		ssh.close()
 			
 			
 	elif reply == "Put" or reply == "put":
-		item = input("File: ")
-		dst = item
+		item = input("Datei: ")
+		Ziel = item
 		ftp_client=ssh.open_sftp()
-		ftp_client.put(item, dst, confirm=False, callback=None)
+		ftp_client.put(item, Ziel, confirm=False, callback=None)
 		ftp_client.close()
 		ssh.close()
 	else:
